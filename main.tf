7ec99ded899e4c2e86779f67254db4c135c2dafc
root <root@ceph03.kolla>
2025-09-16 05:45:39 +0000

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "openstack_compute_instance_v2" "instance" {
   count = "${var.instance_count}"
-  name = format("%s-%03d", var.openstack_instance_name_prefix, count.index + 1)
-  flavor_name = data.openstack_compute_flavor_v2.flavor.name
+  name = format("%s-%02d", var.openstack_instance_name_prefix, count.index + 1)
+  flavor_name = data.openstack_compute_flavor_v2.flavor[count.index].name
   key_pair = var.openstack_key_name
   network {
     name = "private"
@@ -13,23 +13,33 @@ resource "openstack_compute_instance_v2" "instance" {
   source_type = "volume"
   uuid = openstack_blockstorage_volume_v3.os_block[count.index].id
   }
+  metadata = {
+    imageName = "${var.openstack_image_names[count.index]}"
+  }
+  user_data = "#cloud-config\nchpasswd:\n  list: |\n    root:password\n"
 }
 
 resource "openstack_blockstorage_volume_v3" "os_block"{
   count = "${var.instance_count}"
   name = "${var.openstack_instance_name_prefix}-block-${count.index + 1}"
-  size = 100
+  size = "${var.os_disk_size[count.index]}"
   enable_online_resize = true
-  image_id = data.openstack_images_image_v2.image.id
+  image_id = data.openstack_images_image_v2.image[count.index].name
+  timeouts {
+    create = "60m"
+    delete = "30m"
+  }
 }
 
 data "openstack_images_image_v2" "image" {
-  name = var.openstack_image_name 
+  count = "${var.instance_count}"
+  name = "${var.openstack_image_names[count.index]}"
   most_recent = true
 }
 
 data "openstack_compute_flavor_v2" "flavor" {
-  name = var.openstack_flavor_name
+  count = "${var.instance_count}"
+  name = "${var.openstack_flavor_names[count.index]}"
 }
 
 data "openstack_networking_port_v2" "port" {
--- variables.tf
+++ variables.tf
@@ -1,9 +1,21 @@
 variable "openstack_image_name" {
   type = string
+  default = ""
+}
+
+variable "openstack_image_names" {
+  type = list(string)
+  description = "List of Image names by index"
+  default = []
 }
 
 variable "openstack_flavor_name" {
   type = string
+  default = ""
+}
+
+variable "openstack_flavor_names" {
+  type = list(string)
 }
 
 variable "openstack_instance_name_prefix" {
@@ -17,3 +29,10 @@ variable "openstack_key_name" {
 variable "instance_count" {
   type = number
 }
+
+variable "os_disk_size" {
+  type = list(number)
+  description = "List of size by index"
+  default = []
+}
+
